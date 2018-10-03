e5d4b0b36bd06803a42d4f3aba838b7342a3cf3d
Chad <chadwpalm@gmail.com>
2018-10-03 16:01:33 -0700

    Initial Sketch
    Initialization of Master  with sketch to be used for embedded control

---
+++ CompositeOven.ino
@@ -0,0 +1,13 @@
+#include <openGLCD.h>
+#include <SerialFlash.h>
+#include <SD.h>
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
